4fd543537c1af1fd7a69ec790eb1cc89662e0b99
Hadi AmirNahavandi <Hadi.nahavandi2010@gmail.com>
2018-11-01 01:09:48 +0330

    Added shiftAllRemainingWords,getTodayWords and getPlaceWords.

--- app/src/main/java/ir/sweetsoft/wordbox/wordbox/Model/Word.java
+++ app/src/main/java/ir/sweetsoft/wordbox/wordbox/Model/Word.java
@@ -1,8 +1,14 @@
 package ir.sweetsoft.wordbox.wordbox.Model;
 
+import android.content.DialogInterface;
+import android.view.View;
+
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.List;
 
 /**
  * Created by Hadi on 10/03/2017.
@@ -35,5 +41,23 @@ public class Word extends Model {
     public Word()
     {
         super();
+    }
+
+    public static void shiftAllRemainingWords()
+    {
+        List<Word> allwords=new Select().from(Word.class).where("place<33").execute();
+        for (int i=0;i<allwords.size();i++) {
+            allwords.get(i).place++;
+            allwords.get(i).save();
+        }
+
+    }
+    public static List<Word> getTodayWords()
+    {
+        return new Select().from(Word.class).where("place=1 OR place=2 OR place=4 OR place=8 OR place=16 OR place=32").execute();
+    }
+    public static List<Word> getPlaceWords(int PlaceID)
+    {
+        return new Select().from(Word.class).where("place="+PlaceID).execute();
     }
 }
